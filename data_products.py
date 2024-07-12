8bd0e13c7a0ae4a3df069ce59a935c530c14ad77
pennycuda <141153875+pennycuda@users.noreply.github.com>
2024-07-12 16:55:17 -0400

    update script

--- data_products.py
+++ data_products.py
@@ -2,56 +2,19 @@ if __name__ == "__main__":
     import django
     django.setup()
 
-import anndata as ad
-import s3fs
-import h5py
+import json
+import re
+import os
 import pandas as pd
 import yaml
 from data_products.models import DataProduct, Tissue, Assay, Dataset
 from argparse import ArgumentParser
-
-organ_types_file = "organ_types.yaml"
-
-
-def get_tissue_type(tissue_key):
-    organ_dict = yaml.load(open((organ_types_file)), Loader=yaml.BaseLoader)
-    for key in organ_dict:
-        organ_dict[key] = organ_dict[key]["description"]
-    if tissue_key not in organ_dict.keys():
-        print(f"Tissue {tissue_key} not found")
-        tissue_type = None
-    else: 
-        tissue_type = organ_dict[tissue_key]
-    return tissue_type
+from pathlib import Path
 
 
-def open_h5ad_file(access_key_id, secret_access_key, url):
-    s3 = s3fs.S3FileSystem(key = access_key_id, secret = secret_access_key)
-    with s3.open(url, 'rb') as f:
-        h5ad = ad.read_h5ad(f)
-        f.close()
-    return h5ad
-
-
-def open_umap(access_key_id, secret_access_key, url):
-    s3 = s3fs.S3FileSystem(key = access_key_id, secret = secret_access_key)
-    f = s3.open(url)
-    return f
-
-
-def get_datasets(access_key_id, secret_access_key, url):
-    h5ad = open_h5ad_file(access_key_id, secret_access_key, url)
-    uuids = h5ad.obs["dataset"].drop_duplicates()
-    hbmids = h5ad.obs["hubmap_id"].drop_duplicates()
-    dataset_ids = uuids.to_frame().join(hbmids)
-    return dataset_ids
-
-
-def register_datasets(dataset_ids: pd.DataFrame):
+def register_datasets(uuids, hbmids):
     datasets = []
-    for index, row in dataset_ids.iterrows():
-        dataset_uuid = row['dataset']
-        dataset_hbmid = row['hubmap_id']
+    for dataset_uuid, dataset_hbmid in zip(uuids, hbmids):
         dataset = Dataset.objects.get_or_create(
             uuid = dataset_uuid,
             hbmid = dataset_hbmid
@@ -67,43 +30,79 @@ def register_tissue(tissue_type):
     return tissue
 
 
-def register_data_product(data_product, dataset_list, umap, tissue_type):
+def register_data_product(metadata_file, umap_file):
+    metadata = read_metadata(metadata_file)
+    data_product_uuid = metadata["Data Product UUID"]
+    tissue_type = metadata["Tissue"]
+    dataset_uuids = metadata["Dataset UUIDs"]
+    dataset_hbmids = metadata["Dataset HBMIDs"]
+    dataset_list = register_datasets(dataset_uuids, dataset_hbmids)
+    raw_cell_count = metadata["Raw Total Cell Count"]
+    processed_cell_count = metadata["Processed Total Cell Count"]
+    directory_url = f"https://hubmap-data-products.s3.amazonaws.com/{data_product_uuid}"
     data_product = DataProduct.objects.get_or_create(
+        data_product_id = data_product_uuid,
         tissue = register_tissue(tissue_type),
         datasets = dataset_list,
-        download = data_product,
-        umap_plot = umap
+        download = directory_url,
+        umap_plot = umap_file,
+        raw_total_cell_count = raw_cell_count,
+        processed_total_cell_count = processed_cell_count
     )[0]
     data_product.save()
 
 
-def main(tissue_key, access_key_id, secret_access_key):
-    bucket = f"s3://hubmap-data-products/{tissue_key}/"
-    processed_file = f"{tissue_key}_processed.h5ad"
-    raw_file = f"{tissue_key}_raw.h5ad"
-    umap_file = f"{tissue_key}.png"
-    data_product_file_names = [processed_file, raw_file]
-    processed_url = bucket+processed_file
-    raw_url = bucket+raw_file
-    umap_url = bucket+umap_file
-    data_product_urls = [processed_url, raw_url]
-    tissue_type = get_tissue_type(tissue_key)
-    dataset_ids = get_datasets(access_key_id, secret_access_key, raw_url)
-    # processed_file = open_h5ad_file(access_key_id, secret_access_key, processed_url)
-    # raw_file = open_h5ad_file(access_key_id, secret_access_key, raw_url)
-    umap = open_umap(access_key_id, secret_access_key, umap_url)
-    # # h5ads = [processed_file, raw_file]
-    datasets = register_datasets(dataset_ids)
-    for data_product in data_product_urls:
-        register_data_product(data_product, datasets, umap, tissue_type)
+def register_data_products(metadata_list, umap_list):
+    for metadata, umap in zip(metadata_list, umap_list):
+        register_data_product(metadata, umap)
+
+
+def read_metadata(metadata_file):
+    with open(metadata_file, 'r') as json_file:
+        metadata = json.load(metadata_file)
+    return metadata\
+
+
+def find_metadatas(directory):
+    pattern = re.compile(r'.*\.json$')
+    metadatas = find_files(directory, pattern)
+    return metadatas
+
+
+def find_umaps(directory):
+    pattern = re.compile(r'.*\.png$')
+    pngs = find_files(directory, pattern)
+    return pngs
+
+
+def find_files(directory, pattern):
+    json_files = []
+    for root, dirs, files, in os.walk(directory):
+        for filename in files:
+            if pattern.match(filename):
+                json_files.append(os.path.join(root, filename))
+    return json_files
+
+
+def delete_json_file(directory, json_file):
+    try: 
+        os.remove(os.path.join(directory, json_file))
+        print(f"Deleted: {json_file}")
+    except Exception as e:
+        print(f"Error deleting file {json_file}: {e}")
+
+
+def main(directory):
+    metadata_files = find_metadatas(directory)
+    umap_files = find_umaps(directory)
+    register_data_products(metadata_files, umap_files)
+    for file in metadata_files:
+        delete_json_file(directory, file)
 
 
 if __name__ == "__main__":
     p = ArgumentParser()
-    p.add_argument("tissue", type=str)
-    p.add_argument("access_key_id", type=str)
-    p.add_argument("secret_access_key", type=str)
-    
+    p.add_argument("directory", type=Path)
     args = p.parse_args()
 
-    main(args.tissue, args.access_key_id, args.secret_access_key)
+    main(args.directory)
