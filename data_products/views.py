a25d5a52effbd762d7192476c2fee5c1e6cdcdd8
pennycuda <141153875+pennycuda@users.noreply.github.com>
2024-07-15 12:20:28 -0400

    update naming again

--- data_products/views.py
+++ data_products/views.py
@@ -12,8 +12,8 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
-def detail(request, dataProductId):
+def detail(request, data_product_id):
     #return HttpResponse("You're looking at dataProduct %s." % dataProductId)
 
-    product = get_object_or_404(DataProduct, pk=dataProductId)
+    product = get_object_or_404(DataProduct, pk=data_product_id)
     return render(request, "data_products/detail.html", {"product": product})
