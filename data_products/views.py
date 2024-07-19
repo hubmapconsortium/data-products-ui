c10da813eca90327d6a678f3bcdd8542c09a3c04
Sarah <sschultz@cs.cmu.edu>
2024-07-19 13:46:34 -0400

    Add view by tissue type

--- data_products/views.py
+++ data_products/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import DataProduct
+from .models import DataProduct, Tissue
 
 def index(request):
-    latest_data_product_list = DataProduct.objects.order_by("creation_time")[:5]
+    tissue_list = Tissue.objects.order_by("tissuetype")
+    latest_data_product_list=[]
+    for t in tissue_list:
+        latest_data_product_t=DataProduct.objects.filter(tissue=t).order_by("-creation_time")
+        latest_data_product_list.append(latest_data_product_t[0])
     template = loader.get_template("data_products/index.html")
     context = {
         "latest_data_product_list": latest_data_product_list,
@@ -17,3 +21,13 @@ def detail(request, dataProductId):
 
     product = get_object_or_404(DataProduct, pk=dataProductId)
     return render(request, "data_products/detail.html", {"product": product})
+
+def tissue(request, tissuetype):
+
+    tissue = Tissue.objects.filter(tissuetype=tissuetype)
+    tissue_data_product_list = DataProduct.objects.filter(tissue__in=tissue.all()).order_by("-creation_time")
+    template = loader.get_template("data_products/index.html")
+    context = {
+        "latest_data_product_list": tissue_data_product_list,
+    }
+    return HttpResponse(template.render(context, request))
