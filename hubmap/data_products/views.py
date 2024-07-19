6303bcf9963260f2acbc83166d1f05998a034c5d
Sarah <sschultz@cs.cmu.edu>
2024-07-19 13:46:34 -0400

    Add view by tissue type

--- hubmap/data_products/views.py
+++ hubmap/data_products/views.py
@@ -2,11 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
-from .models import DataProduct
+from .models import DataProduct, Tissue
 
 
 def index(request):
-    latest_data_product_list = DataProduct.objects.order_by("creation_time")[:5]
+    tissue_list = Tissue.objects.order_by("tissuetype")
+    latest_data_product_list=[]
+    for t in tissue_list:
+        latest_data_product_t=DataProduct.objects.filter(tissue=t).order_by("-creation_time")
+        latest_data_product_list.append(latest_data_product_t[0])
     template = loader.get_template("data_products/index.html")
     context = {
         "latest_data_product_list": latest_data_product_list,
@@ -19,3 +23,13 @@ def detail(request, data_product_id):
 
     product = get_object_or_404(DataProduct, pk=data_product_id)
     return render(request, "data_products/detail.html", {"product": product})
+
+def tissue(request, tissuetype):
+
+    tissue = Tissue.objects.filter(tissuetype=tissuetype)
+    tissue_data_product_list = DataProduct.objects.filter(tissue__in=tissue.all()).order_by("-creation_time")
+    template = loader.get_template("data_products/index.html")
+    context = {
+        "latest_data_product_list": tissue_data_product_list,
+    }
+    return HttpResponse(template.render(context, request))
