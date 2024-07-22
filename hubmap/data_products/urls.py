c71f813ff74f7dc41b0da3667bdaa4c8a9674336
root <root@ip-172-31-64-11.ec2.internal>
2024-07-22 23:28:16 +0000

    Add comma

--- hubmap/data_products/urls.py
+++ hubmap/data_products/urls.py
@@ -8,5 +8,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<uuid:dataProductId>/", views.detail, name="detail"),
-    path("<str:tissuetype>/", views.tissue, name="tissue") 
+    path("<str:tissuetype>/", views.tissue, name="tissue"), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
