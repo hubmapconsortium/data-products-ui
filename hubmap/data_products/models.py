340b65b696b3a160f501c893b25d1ef8284ffb7c
Sarah <sschultz@cs.cmu.edu>
2024-08-31 00:00:27 -0400

    Add ManyToManyField to DataProduct model, default value for file sizes

--- hubmap/data_products/models.py
+++ hubmap/data_products/models.py
@@ -68,14 +68,17 @@ class DataProduct(models.Model):
 
     creation_time = models.DateTimeField(auto_now_add=True)
     tissue = models.ForeignKey(Tissue, on_delete=models.CASCADE)
+
+    dataSets = models.ManyToManyField(Dataset, blank=True)
+    
     download = models.URLField(null=True, blank=True)
     umap_plot = models.ImageField(null=True, blank=True, upload_to="images/")
     raw_total_cell_count = models.PositiveIntegerField(null=True, blank=True)
     processed_total_cell_count = models.PositiveIntegerField(null=True, blank=True)
     processed_cell_type_counts = models.JSONField(blank=True)
     raw_cell_type_counts = models.JSONField(blank=True)
-    raw_file_size_bytes = models.PositiveBigIntegerField(blank=True)
-    processed_file_sizes_bytes = models.PositiveBigIntegerField(blank=True)
+    raw_file_size_bytes = models.PositiveBigIntegerField(blank=True, default=0)
+    processed_file_sizes_bytes = models.PositiveBigIntegerField(blank=True, default=0)
     assay = models.ForeignKey(Assay, on_delete=models.CASCADE, default=Assay.get_default_pk)
 
     #link to this data product's shiny app
