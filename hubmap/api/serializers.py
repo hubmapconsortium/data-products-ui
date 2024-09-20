9b9415a69b3bb3f24ad95e578e50c15b2f0bb054
Sarah <sschultz@cs.cmu.edu>
2024-09-19 23:39:36 -0400

    Delete unneeded calls

--- hubmap/api/serializers.py
+++ hubmap/api/serializers.py
@@ -2,18 +2,6 @@ from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 from data_products.models import *
 
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
-
 class TissueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tissue
--- hubmap/api/views.py
+++ hubmap/api/views.py
@@ -8,23 +8,6 @@ from rest_framework.decorators import api_view
 from .serializers import *
 from data_products.models import *
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all().order_by('name')
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
 
 def data_product_list(request):
 
@@ -33,28 +16,12 @@ def data_product_list(request):
         serializer = DataProductSerializer(dataproducts, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = DataProductSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
 def tissue_list(request):
     
     if request.method == 'GET':
         tissues = Tissue.objects.all()
         serializer = TissueSerializer(tissues, many=True)
         return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = TissueSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
 
 def dataset_list(request):
 
@@ -63,32 +30,17 @@ def dataset_list(request):
         serializer = DatasetSerializer(datasets, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = DatasetSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
 
 def assay_list(request):
+
     if request.method == 'GET':
         assays = Assay.objects.all()
         serializer = AssaySerializer(assays, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = AssaySerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
 
 def data_product_detail(request, data_product_id):
-    """
-    Retrieve, update or delete a data product.
-    """
+
     try:
         product = DataProduct.objects.get(data_product_id=data_product_id)
     except DataProduct.DoesNotExist:
@@ -98,22 +50,9 @@ def data_product_detail(request, data_product_id):
         serializer = DataProductSerializer(product)
         return JsonResponse(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = DataProductSerializer(product, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        product.delete()
-        return HttpResponse(status=204)
 
 def assay_detail(request, assayName):
-    """
-    Retrieve, update or delete an assay type
-    """
+
     try:
         assay = Assay.objects.get(assayName=assayName)
     except Assay.DoesNotExist:
@@ -123,22 +62,8 @@ def assay_detail(request, assayName):
         serializer = AssaySerializer(assay)
         return JsonResponse(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = AssaySerializer(assay, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        assay.delete()
-        return HttpResponse(status=204)
-
 def tissue_detail(request, tissuetype):
-    """
-    Retrieve, update or delete a tissue type
-    """
+
     try:
         tissue = Tissue.objects.get(tissuetype=tissuetype)
     except Tissue.DoesNotExist:
@@ -148,22 +73,8 @@ def tissue_detail(request, tissuetype):
         serializer = TissueSerializer(tissue)
         return JsonResponse(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = TissueSerializer(tissue, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        assay.delete()
-        return HttpResponse(status=204)
-
 def dataset_detail(request, uuid):
-    """
-    Retrieve, update or delete a dataset
-    """
+
     try:
         dataset = Dataset.objects.get(uuid=uuid)
     except Dataset.DoesNotExist:
@@ -172,15 +83,3 @@ def dataset_detail(request, uuid):
     if request.method == 'GET':
         serializer = DatasetSerializer(dataset)
         return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = DatasetSerializer(dataset, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        assay.delete()
-        return HttpResponse(status=204)
