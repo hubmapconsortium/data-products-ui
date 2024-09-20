f10f5094809e0c7746588f6c3f458f929d12ad9a
Sarah <sschultz@cs.cmu.edu>
2024-09-19 23:39:16 -0400

    API index lists data products

--- hubmap/api/urls.py
+++ hubmap/api/urls.py
@@ -6,12 +6,8 @@ from rest_framework import routers
 
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
 urlpatterns = [
-    path("", include(router.urls)),
+    path("", views.data_product_list),
     path("data_products/", views.data_product_list),
     path("tissues", views.tissue_list),
     path("datasets", views.dataset_list),
