a3361ecbe1e7307b5efc0529da1e99eaee9756c9
Sarah <sschultz@cs.cmu.edu>
2024-09-27 01:32:10 -0400

    Show most recent for each assay and tissue

--- hubmap/data_products/views.py
+++ hubmap/data_products/views.py
@@ -2,15 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
-from .models import DataProduct, Tissue
+from .models import DataProduct, Tissue, Assay
 
 
 def index(request):
     tissue_list = Tissue.objects.order_by("tissuetype")
+    assay_list = Assay.objects.order_by("assayName")
     latest_data_product_list=[]
     for t in tissue_list:
-        latest_data_product_t=DataProduct.objects.filter(tissue=t).order_by("-creation_time")
-        latest_data_product_list.append(latest_data_product_t[0])
+        for a in assay_list:
+            latest_data_product_t=DataProduct.objects.filter(tissue=t, assay=a).order_by("-creation_time")
+            if(len(latest_data_product_t)>0):
+                latest_data_product_list.append(latest_data_product_t[0])
     template = loader.get_template("data_products/index.html")
     context = {
         "latest_data_product_list": latest_data_product_list,
@@ -25,6 +28,8 @@ def detail(request, data_product_id):
     assay = product.assay
     if assay.assayName=="rna-seq":
         template = loader.get_template("data_products/rna-detail.html")
+    elif assay.assayName=="atac":
+        template = loader.get_template("data_products/atac-detail.html")
     else:
         template = loader.get_template("data_products/detail.html")
     context = {"product": product,}
