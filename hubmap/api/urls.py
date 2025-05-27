9c374242fec1e9ef5b17c249a36373d6cf719825
pennycuda <141153875+pennycuda@users.noreply.github.com>
2025-05-27 11:13:40 -0400

    Add status view

--- hubmap/api/urls.py
+++ hubmap/api/urls.py
@@ -7,6 +7,7 @@ from rest_framework import routers
 from . import views
 
 urlpatterns = [
+    path("status/", views.status_view),
     path("", views.data_product_list),
     path("data_products/", views.data_product_list),
     path("tissues", views.tissue_list),
@@ -16,5 +17,5 @@ urlpatterns = [
     path("assays/<str:assayName>/", views.assay_detail),
     path("tissues/<str:tissuetype>/", views.tissue_detail),
     path("datasets/<str:uuid>/", views.dataset_detail),
-    path("data_products/<str:tissuetype>/", views.data_products_by_tissue)
+    path("data_products/<str:tissuetype>/", views.data_products_by_tissue),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
