c0d65cf360fe19321ec0a15fe63753eb79d50f3e
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-03-26 11:28:32 -0400

    add the redirect in urls

--- hubmap/data_products_ui/urls.py
+++ hubmap/data_products_ui/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 #from rest_framework import routers
 
 from . import views
@@ -13,6 +14,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("admin/", admin.site.urls),
+    path("data_products/", RedirectView.as_view(url="https://portal.hubmapconsortium.org/integrated-maps", permanent=True)),
     path("api/", include("api.urls")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
